dd75fddab3adbbd370dd89a504c1fa3580eee214
ThreeToads <ivan.kokin.465@gmail.com>
2024-12-24 05:16:10 +0300

    fix(blogicum): add files for solution task part 3

--- blogicum/env.py
+++ blogicum/env.py
@@ -1,2 +1,3 @@
-SECRET_KEY = '''django-insecure--emrv-4wb*%ic3nvg#ub+=cv1vg_f5$0f^zg+kvv*uhsj@mijz4'''
+SECRET_KEY = '''django-insecure--emrv-4wb*%ic3nvg#ub
++=cv1vg_f5$0f^zg+kvv*uhsj@mijz4'''
 DEBUG = True
